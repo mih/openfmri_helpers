2985c3ddf5203d8417d91ca5d02afec8775979a1
Michael Hanke <michael.hanke@gmail.com>
2014-12-08 16:21:29 +0100

    ENH: Implement --via for align2subjtmpl

--- openfmri/cmdline/cmd_align2subjtmpl.py
+++ openfmri/cmdline/cmd_align2subjtmpl.py
@@ -43,10 +43,10 @@ def setup_parser(parser):
         mask for each aligned volume. The minimum projections through
         time of these masks are available as QA output. This parameter
         has no influence on the actual alignment.""")
-    parser.add_argument('--input-subjtmpl', type=hlp.arg2bool,
-        help="""Whether to treat the input as a subject template's head image.
-        This will cause the algorithm to re-use existing brain masks and output
-        results into a template-like directory structure.""")
+    parser.add_argument('--via',
+        help="""Label of a subject template to look up a transformed
+        reference image in. This can be used of direct alignment is too
+        unstable.""")
 
 import sys
 import os                                    # system functions
@@ -56,83 +56,6 @@ import nipype.interfaces.utility as util
 import nipype.interfaces.fsl as fsl
 from nipype.interfaces.utility import Function
 from ..nipype_helpers import *
-
-
-def align_file(wf, label, subj, subj_sink, data, reference,
-               tmpl_brain_mask, fgthresh):
-    subj_aligned = pe.Node(
-            name='sub%.3i_aligned' % subj,
-            interface=util.Merge(len(data.outputs.out_paths)))
-    # do the same thing for each input file
-    for i, input in enumerate(sorted(data.outputs.out_paths)):
-        basename = os.path.basename(input)
-        basename = basename[:basename.index('.')]
-        hash = hashlib.md5(input).hexdigest()
-        sink = pe.Node(
-                interface=nio.DataSink(
-                    parameterization=False,
-                    base_directory=os.path.abspath(os.path.dirname(input)),
-                    regexp_substitutions=[
-                        ('/[^/]*\.par', '_%s.txt' % label),
-                        ('/[^/]*\.nii', '_%s.nii' % label),
-                        ('/[^/]*\.mat', '_%s.mat' % label),
-                    ]),
-                name="sub%.3i_%i_sink_%s" % (subj, i, hash),
-                overwrite=True)
-
-        # 4D alignment to template
-        align4d = pe.Node(
-                name='sub%.3i_%i_align4d_%s' % (subj, i, hash),
-                interface=fsl.MCFLIRT(
-                    cost='corratio',
-                    ref_file=reference,
-                    save_plots=True,
-                    stages=4))
-        align4d.inputs.in_file = input
-        wf.connect(align4d, 'out_file', sink, '%s.@out' % basename)
-        wf.connect(align4d, 'par_file', sink, 'qa.%s_moest.@out' % basename)
-        # a bit of QA: compute a mask indicating voxels that are
-        # non-background for the entire during of the timeseries
-        # non-background is defined as less the X% of the robust range
-        foregroundmask = pe.Node(
-            name='sub%.3i_%i_foregroundmask_%s' % (subj, i, hash),
-            interface=fsl.Threshold(
-                thresh=fgthresh,
-                args=' -bin -Tmin',
-                use_robust_range=True,
-                output_datatype='char'))
-        wf.connect(align4d, 'out_file', foregroundmask, 'in_file')
-        wf.connect(foregroundmask, 'out_file',
-                   sink, 'qa.%s_fgmask.@out' % basename)
-        wf.connect(foregroundmask, 'out_file',
-                   subj_aligned, 'in%i' % (i + 1))
-    # merge subj fgmasks and store for QA
-    subj_merge_aligned = pe.Node(
-            name='sub%.3i_merge_aligned' % subj,
-            interface=fsl.Merge(dimension='t'))
-    wf.connect(subj_aligned, 'out', subj_merge_aligned, 'in_files')
-    wf.connect(subj_merge_aligned, 'merged_file',
-               subj_sink, 'qa.fgmasks.@out')
-    jointfgmask = pe.Node(
-        name='sub%.3i_jointfgmask' % (subj,),
-        interface=fsl.maths.MathsCommand(
-            args=' -Tmin -bin ',
-            output_datatype='char'))
-    wf.connect(subj_merge_aligned, 'merged_file',
-               jointfgmask, 'in_file')
-    wf.connect(jointfgmask, 'out_file',
-               subj_sink, 'qa.jointfgmask.@out')
-    if not tmpl_brain_mask is None:
-        jointfgbrainmask = pe.Node(
-            name='sub%.3i_jointfgbrainmask' % (subj,),
-            interface=fsl.maths.MathsCommand(
-                args=' -mul "%s" ' % tmpl_brain_mask,
-                output_datatype='char'))
-        wf.connect(jointfgmask, 'out_file',
-                   jointfgbrainmask, 'in_file')
-        wf.connect(jointfgbrainmask, 'out_file',
-                   subj_sink, 'qa.jointfgbrainmask.@out')
-    return wf
 
 def run(args):
     # label of the group template -- used to look up config options
@@ -153,14 +76,10 @@ def run(args):
                                    cli_input=args.input_expression)
     template = hlp.get_cfg_option(cfg_section, 'template',
                                   cli_input=args.template)
+    via = hlp.get_cfg_option(cfg_section, 'via template',
+                             cli_input=args.via)
     fgthresh = float(hlp.get_cfg_option(cfg_section, 'foreground threshold',
                                       cli_input=args.fgthresh, default=5.0))
-
-    input_subjtmpl=hlp.arg2bool(
-            hlp.get_cfg_option(cfg_section,
-                               'input is subject template',
-                               cli_input=args.input_subjtmpl,
-                               default=False))
 
     for subj in subjects:
         subj_sink = pe.Node(
@@ -191,7 +110,83 @@ def run(args):
             # no data, nothing to do
             continue
 
-        wf = align_file(wf, label, subj, subj_sink, result, reference,
-                        tmpl_brain_mask, fgthresh)
+        subj_aligned = pe.Node(
+                name='sub%.3i_aligned' % subj,
+                interface=util.Merge(len(result.outputs.out_paths)))
+        # do the same thing for each input file
+        for i, input in enumerate(sorted(result.outputs.out_paths)):
+            basename = os.path.basename(input)
+            basename = basename[:basename.index('.')]
+            hash = hashlib.md5(input).hexdigest()
+            sink = pe.Node(
+                    interface=nio.DataSink(
+                        parameterization=False,
+                        base_directory=os.path.abspath(os.path.dirname(input)),
+                        regexp_substitutions=[
+                            ('/[^/]*\.par', '_%s.txt' % label),
+                            ('/[^/]*\.nii', '_%s.nii' % label),
+                            ('/[^/]*\.mat', '_%s.mat' % label),
+                        ]),
+                    name="sub%.3i_%i_sink_%s" % (subj, i, hash),
+                    overwrite=True)
+
+            # 4D alignment to template
+            align4d = pe.Node(
+                    name='sub%.3i_%i_align4d_%s' % (subj, i, hash),
+                    interface=fsl.MCFLIRT(
+                        cost='corratio',
+                        save_plots=True,
+                        stages=4))
+            align4d.inputs.in_file = input
+            wf.connect(align4d, 'out_file', sink, '%s.@out' % basename)
+            wf.connect(align4d, 'par_file', sink, 'qa.%s_moest.@out' % basename)
+            # look up an initial xfm to contrain the search
+            if not via is None:
+                reference = opj(dsdir, 'sub%.3i' % subj, 'templates',
+                                 via, 'in_%s' % template, 'head.nii.gz')
+                if not os.path.exists(reference):
+                    raise ValueError('"via template" does not exist')
+            align4d.inputs.ref_file = reference
+            # a bit of QA: compute a mask indicating voxels that are
+            # non-background for the entire during of the timeseries
+            # non-background is defined as less the X% of the robust range
+            foregroundmask = pe.Node(
+                name='sub%.3i_%i_foregroundmask_%s' % (subj, i, hash),
+                interface=fsl.Threshold(
+                    thresh=fgthresh,
+                    args=' -bin -Tmin',
+                    use_robust_range=True,
+                    output_datatype='char'))
+            wf.connect(align4d, 'out_file', foregroundmask, 'in_file')
+            wf.connect(foregroundmask, 'out_file',
+                       sink, 'qa.%s_fgmask.@out' % basename)
+            wf.connect(foregroundmask, 'out_file',
+                       subj_aligned, 'in%i' % (i + 1))
+        # merge subj fgmasks and store for QA
+        subj_merge_aligned = pe.Node(
+                name='sub%.3i_merge_aligned' % subj,
+                interface=fsl.Merge(dimension='t'))
+        wf.connect(subj_aligned, 'out', subj_merge_aligned, 'in_files')
+        wf.connect(subj_merge_aligned, 'merged_file',
+                   subj_sink, 'qa.fgmasks.@out')
+        jointfgmask = pe.Node(
+            name='sub%.3i_jointfgmask' % (subj,),
+            interface=fsl.maths.MathsCommand(
+                args=' -Tmin -bin ',
+                output_datatype='char'))
+        wf.connect(subj_merge_aligned, 'merged_file',
+                   jointfgmask, 'in_file')
+        wf.connect(jointfgmask, 'out_file',
+                   subj_sink, 'qa.jointfgmask.@out')
+        if not tmpl_brain_mask is None:
+            jointfgbrainmask = pe.Node(
+                name='sub%.3i_jointfgbrainmask' % (subj,),
+                interface=fsl.maths.MathsCommand(
+                    args=' -mul "%s" ' % tmpl_brain_mask,
+                    output_datatype='char'))
+            wf.connect(jointfgmask, 'out_file',
+                       jointfgbrainmask, 'in_file')
+            wf.connect(jointfgbrainmask, 'out_file',
+                       subj_sink, 'qa.jointfgbrainmask.@out')
     return wf
 
